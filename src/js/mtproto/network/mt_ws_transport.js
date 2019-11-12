96048d242642a4b2933d5322a56e1acca999b8c9
O.Tsenilov <o.tsenilov@gmail.com>
2019-11-12 12:13:43 +0200

    https://imgur.com/a/GiDHqUM
    mt_ws_transport is now capable of managing multiple url connections, just use mt_ws_transport(any_url, data_buffer)

--- src/js/mtproto/network/mt_ws_transport.js
+++ src/js/mtproto/network/mt_ws_transport.js
@@ -4,28 +4,25 @@
 
 import {mt_inob_send, mt_inob_recv} from "./mt_inob_codec"
 
-var transportation_socket;
+var transportation_streams = new Map();
+
 var high_level_processor;
 var high_level_context;
-var transportation_init = false;
-var transportation_establishing = false;
-
-var transportation_queue = [];
-var transportation_queue_len = 0;
 
 function mt_init_transportation(url)
 {
-    transportation_socket = new WebSocket(url, "binary");
+    var transportation_socket = new WebSocket(url, "binary");
     transportation_socket.binaryType = "arraybuffer";
 
     transportation_socket.onopen = function(ev)
     {
-        for (var i = 0; i < transportation_queue_len; i++) {
-            mt_inob_send(transportation_socket, transportation_queue[i], transportation_queue[i].byteLength);
+        var transportation_stream = transportation_streams.get(url);
+        for (var i = 0; i < transportation_stream.transportation_queue_len; i++) {
+            mt_inob_send(transportation_socket, transportation_stream.transportation_queue[i], transportation_stream.transportation_queue[i].byteLength);
             //console.log("transported buffer / 0");
             //console.log(transportation_queue[i].byteLength);
         }
-        transportation_init = true;
+        transportation_stream.transportation_init = true;
     };
 
     transportation_socket.onmessage = function(ev)
@@ -49,6 +46,14 @@ function mt_init_transportation(url)
         }
         console.log('code: ' + ev.code + ' reason: ' + ev.reason);
     };
+    
+    transportation_streams.set(url, {
+                                        transportation_socket: transportation_socket,
+                                        transportation_init: false,
+                                        transportation_establishing: false,
+                                        transportation_queue: [],
+                                        transportation_queue_len: 0
+                                    });
 }
 
 export function mt_ws_set_processor(processor, context)
@@ -59,23 +64,35 @@ export function mt_ws_set_processor(processor, context)
 
 export function mt_ws_transport(url, buffer)
 {
+    var transportation_init = false;
+    var transportation_establishing = false;
+    var transportation_stream = transportation_streams.get(url);
+    if(transportation_stream != undefined)
+    {
+        transportation_init = transportation_stream.transportation_init;
+        transportation_establishing = transportation_stream.transportation_establishing;
+    }
+
     if(!transportation_init)
     {
         if(!transportation_establishing)
         {
             mt_init_transportation(url);
-            transportation_queue[transportation_queue_len] = buffer;
-            ++transportation_queue_len;
-            transportation_establishing = true;
+            transportation_stream = transportation_streams.get(url);
+
+            transportation_stream.transportation_queue[transportation_stream.transportation_queue_len] = buffer;
+            ++(transportation_stream.transportation_queue_len);
+            transportation_stream.transportation_establishing = true;
             //console.log("added to transportation_queue / 0");
         }
         else
         {
+            transportation_stream.transportation_queue[transportation_queue_len] = buffer;
+            ++(transportation_stream.transportation_queue_len);
             //console.log("added to transportation_queue");
-            transportation_queue[transportation_queue_len] = buffer;
-            ++transportation_queue_len;
         }
         return;
     }
-    mt_inob_send(transportation_socket, buffer, buffer.byteLength);
+    mt_inob_send(transportation_stream.transportation_socket, buffer, buffer.byteLength);
+    //console.log("transported buffer");
 }
