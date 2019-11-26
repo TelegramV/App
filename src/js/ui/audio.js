0baf29d72261c45db2a713a5d7e42334a4550fea
Cool_boy <Coolboy.mc1@gmail.com>
2019-11-27 01:19:49 +0200

    Лови (я не тестив)

--- src/js/ui/audio.js
+++ src/js/ui/audio.js
@@ -1,10 +1,24 @@
-const audios = {
+const sounds = {
     "notification": "/static/notification.mp3",
     "out": "/static/sound_out.mp3",
     "in": "/static/sound_in.mp3",
 }
 
+let activeAudio = [];
+
 export function playSound(sound) {
-    if(!audios[sound]) throw new Error("No such sound: " + sound)
-    new Audio(audios[sound]).play()
+    if (!sounds[sound]) throw new Error("No such sound: " + sound)
+    let audio = new Audio(sounds[sound]);
+    audio.play();
+}
+
+export function playAudio(audio) {
+    if (!audio) throw new Error("No audio provided!");
+    audioRegistry.push(audio);
+    audio.play();
+}
+
+export function clear() {
+	activeAudio.forEach(e=>e.stop());
+	activeAudio.clear();
 }
