176b1d514612611f6fbc0bf16fe724bc12e188dc
Cool_boy <Coolboy.mc1@gmail.com>
2020-01-23 19:05:20 +0200

    no-chat

--- src/js/ui/pages/main/components/chat/chatComponent.js
+++ src/js/ui/pages/main/components/chat/chatComponent.js
@@ -6,27 +6,6 @@ import BubblesComponent from "./bubblesComponent"
 import AppSelectedDialog from "../../../../../api/dialogs/selectedDialog"
 import {ChatInputComponent} from "./chatInput/chatInputComponent";
 
-const NoChatTemplate = (
-    <div id="noChat">
-        <div className="placeholder tgico tgico-chatsplaceholder"/>
-        <div className="text"><p>Open Chat</p> <p>or create a new one</p></div>
-        <div className="buttons">
-            <div className="button-wrapper">
-                <div className="button rp"><i className="tgico tgico-newprivate"/></div>
-                <p>Private</p>
-            </div>
-            <div className="button-wrapper">
-                <div className="button rp"><i className="tgico tgico-newgroup"/></div>
-                <p>Group</p>
-            </div>
-            <div className="button-wrapper">
-                <div className="button rp"><i className="tgico tgico-newchannel"/></div>
-                <p>Channel</p>
-            </div>
-        </div>
-    </div>
-)
-
 /**
  * CRITICAL: never rerender this component!
  */
@@ -40,18 +19,38 @@ class ChatComponent extends Component {
 
     h() {
         return (
-            <div id="chat">
-                <div id="topbar">
-                    <ChatInfoComponent/>
-                    <Pinned/>
-                    <div className="btn-icon rp rps tgico-search"/>
-                    <div className="btn-icon rp rps tgico-more"/>
+            <div class="chat-wrapper">
+                <div id="noChat">
+                    <div className="placeholder tgico tgico-chatsplaceholder"/>
+                    <div className="text"><p>Open Chat</p> <p>or create a new one</p></div>
+                    <div className="buttons">
+                        <div className="button-wrapper">
+                            <div className="button rp"><i className="tgico tgico-newprivate"/></div>
+                            <p>Private</p>
+                        </div>
+                        <div className="button-wrapper">
+                            <div className="button rp"><i className="tgico tgico-newgroup"/></div>
+                            <p>Group</p>
+                        </div>
+                        <div className="button-wrapper">
+                            <div className="button rp"><i className="tgico tgico-newchannel"/></div>
+                            <p>Channel</p>
+                        </div>
+                    </div>
                 </div>
+                <div id="chat" css-display="none">
+                    <div id="topbar">
+                        <ChatInfoComponent/>
+                        <Pinned/>
+                        <div className="btn-icon rp rps tgico-search"/>
+                        <div className="btn-icon rp rps tgico-more"/>
+                    </div>
 
-                <LoaderComponent id="messages-wrapper-messages-loader" full={true} show={true}/>
+                    <LoaderComponent id="messages-wrapper-messages-loader" full={true} show={true}/>
 
-                <BubblesComponent/>
-                <ChatInputComponent/>
+                    <BubblesComponent/>
+                    <ChatInputComponent/>
+                </div>
             </div>
         )
     }
