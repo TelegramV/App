e6bbc3009a429bc6b931fbbd54768bc2f882e9f8
O.Tsenilov <o.tsenilov@gmail.com>
2020-02-15 21:24:24 +0200

    final

--- src/js/mtproto/crypto/mt_srp/mt_srp.js
+++ src/js/mtproto/crypto/mt_srp/mt_srp.js
@@ -222,7 +222,7 @@ function PH2(password, salt1, salt2)
 export default function mt_srp_check_password(g, p, salt1, salt2, srp_id, srp_B, /*secure_random, */password)
 {
     console.log("started srp pass check");
-    console.log(password);
+    //console.log(password);
     //console.log("----- DURKA 1 -----");
     //console.log(p);
 
@@ -246,18 +246,18 @@ export default function mt_srp_check_password(g, p, salt1, salt2, srp_id, srp_B,
     var x = PH2(sjcl.codec.utf8String.toBits(password), sjcl.codec.bytes.toBits(salt1), sjcl.codec.bytes.toBits(salt2));
 
     var p_bits = sjcl.codec.bytes.toBits(p);
-    console.log("PRIME_BYTES: ");
-    console.log(p.length);
+    //console.log("PRIME_BYTES: ");
+    //console.log(p.length);
     //AL??
-    console.log(sjcl.codec.hex.fromBits(p_bits));
+    //console.log(sjcl.codec.hex.fromBits(p_bits));
     var big_g = new BigInteger(g.toString(), 10);
     var big_x = new BigInteger(sjcl.codec.hex.fromBits(x), 16);
     var big_p = new BigInteger(sjcl.codec.hex.fromBits(p_bits), 16);
 
     var srp_B_bits = sjcl.codec.bytes.toBits(srp_B);
     var g_b = new BigInteger(sjcl.codec.hex.fromBits(srp_B_bits), 16);
-    console.log("srp_B checks...");
-    console.log(g_b.toString(16));
+    //console.log("srp_B checks...");
+    //console.log(g_b.toString(16));
     //sb > 0 and p - sb > 0
     if(g_b.compareTo(BigInteger.ONE) < 0)
     {
@@ -270,8 +270,8 @@ export default function mt_srp_check_password(g, p, salt1, salt2, srp_id, srp_B,
     }
 
 
-    console.log("----- DURKA 1.5 -----");
-    console.log(big_p.toByteArray());
+    //console.log("----- DURKA 1.5 -----");
+    //console.log(big_p.toByteArray());
     var v = big_g.modPow(big_x, big_p);
 
     var big_k = new BigInteger(sjcl.codec.hex.fromBits(k), 16);
@@ -285,17 +285,17 @@ export default function mt_srp_check_password(g, p, salt1, salt2, srp_id, srp_B,
     var big_a = new BigInteger(sjcl.codec.hex.fromBits(a), 16);
 
     var g_a = big_g.modPow(big_a, big_p);
-    console.log("--- g_a p DEBUG ---");
-    console.log(g_a);
-    console.log("----- DURKA 2 -----");
-    console.log(big_p.length);
-    console.log(new Uint8Array(big_p.toByteArray()));
-    console.log("hard cmp:");
+    //console.log("--- g_a p DEBUG ---");
+    //console.log(g_a);
+    //console.log("----- DURKA 2 -----");
+    //console.log(big_p.length);
+    //console.log(new Uint8Array(big_p.toByteArray()));
+    //console.log("hard cmp:");
     //var durk = new BigInteger("c71caeb9c6b1c9048e6c522f70f13f73980d40238e3e21c14934d037563d930f48198a0aa7c14058229493d22530f4dbfa336f6e0ac925139543aed44cce7c3720fd51f69458705ac68cd4fe6b6b13abdc9746512969328454f18faf8c595f642477fe96bb2a941d5bcd1d4ac8cc49880708fa9b378e3c4f3a9060bee67cf9a4a4a695811051907e162753b56b0f6b410dba74d8a84b2a14b3144e0ef1284754fd17ed950d5965b4b9dd46582db1178d169c6bc465b0d6ff9ca3928fef5b9ae4e418fc15e83ebea0f87fa9ff5eed70050ded2849f47bf959d956850ce929851f0d8115f635b105ee2e4e15d04b2454bf6f4fadf034b10403119cd8e3b92fcc5b", 16);
     //console.log(durk.toByteArray());
     //console.log(durk.toString(16));
-    console.log(sjcl.codec.hex.fromBits(sjcl.codec.bytes.toBits(big_p.toByteArray())));
-    console.log("checking exp cf...");
+    //console.log(sjcl.codec.hex.fromBits(sjcl.codec.bytes.toBits(big_p.toByteArray())));
+    //console.log("checking exp cf...");
     var dexp_1984 = new BigInteger("1751908409537131537220509645351687597690304110853111572994449976845956819751541616602568796259317428464425605223064365804210081422215355425149431390635151955247955156636234741221447435733643262808668929902091770092492911737768377135426590363166295684370498604708288556044687341394398676292971255828404734517580702346564613427770683056761383955397564338690628093211465848244049196353703022640400205739093118270803778352768276670202698397214556629204420309965547056893233608758387329699097930255380715679250799950923553703740673620901978370802540218870279314810722790539899334271514365444369275682816", 10);
     
     if(g_a.compareTo(BigInteger.ONE) <= 0)
@@ -316,7 +316,7 @@ export default function mt_srp_check_password(g, p, salt1, salt2, srp_id, srp_B,
         console.log("g_a >= big_p - dexp_1984");
     }
 
-    console.log("-------------------\n");
+    //console.log("-------------------\n");
 
     //WARNING TODO!!!!
     //man ssl -> BN_cmp() returns -1 if a < b, 0 if a == b and 1 if a > b. BN_ucmp() is the same using the absolute values of a and b.
@@ -332,11 +332,11 @@ export default function mt_srp_check_password(g, p, salt1, salt2, srp_id, srp_B,
         console.log("ZERO CHECK OP OK");
     }
 
-    console.log("checking t/p...");
-    console.log("----");
-    console.log(t);
-    console.log(t.toString(16));
-    console.log("----");
+    //console.log("checking t/p...");
+    //console.log("----");
+    //console.log(t);
+    //console.log(t.toString(16));
+    //console.log("----");
     //var dexp_1984 = new BigInteger("1751908409537131537220509645351687597690304110853111572994449976845956819751541616602568796259317428464425605223064365804210081422215355425149431390635151955247955156636234741221447435733643262808668929902091770092492911737768377135426590363166295684370498604708288556044687341394398676292971255828404734517580702346564613427770683056761383955397564338690628093211465848244049196353703022640400205739093118270803778352768276670202698397214556629204420309965547056893233608758387329699097930255380715679250799950923553703740673620901978370802540218870279314810722790539899334271514365444369275682816", 10);
     
     if(t.compareTo(BigInteger.ONE) <= 0)
@@ -357,16 +357,16 @@ export default function mt_srp_check_password(g, p, salt1, salt2, srp_id, srp_B,
         console.log("t >= big_p - dexp_1984");
     }
 
-    console.log("-------------------\n");
+    //console.log("-------------------\n");
 
     //u := H(g_a | g_b)
     
-    console.log("DURKA 3");
+    //console.log("DURKA 3");
     
     ///console.log(g_a.toString(16));
     ///console.log(g_a.toString(16).slice((257 - 256) * 2));
     
-    console.log(g_a.toByteArray());
+    //console.log(g_a.toByteArray());
     console.log(g_a.toString(16));
     var g_a_bits = sjcl.codec.hex.toBits(g_a.toString(16));
     var u = H(sjcl.bitArray.concat(g_a_bits, srp_B_bits));
@@ -401,8 +401,8 @@ export default function mt_srp_check_password(g, p, salt1, salt2, srp_id, srp_B,
 
     var M1_bytes = sjcl.codec.bytes.fromBits(M1);
     var g_a_bytes = sjcl.codec.bytes.fromBits(g_a_bits);
-    console.log("M1:" + new Uint8Array(M1_bytes));
-    console.log("A: " + new Uint8Array(g_a_bytes));
+    //console.log("M1:" + new Uint8Array(M1_bytes));
+    //console.log("A: " + new Uint8Array(g_a_bytes));
 
     return {
         srp_id: srp_id,
