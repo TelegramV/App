219d99c9ac31f71f235a5697bdf146a46d3f6ba7
Davyd Kohut <expexes@gmail.com>
2020-05-04 22:47:36 +0300

    update license

--- src/js/MTProto/Cryptography/mt_inob_codec.js
+++ src/js/MTProto/Cryptography/mt_inob_codec.js
@@ -1,7 +1,21 @@
-// //MTProto Intermediate Obfuscated Codec W.I.P.
-// //Copyright 2019 Oleg Tsenilov
-// //https://github.com/OTsenilov
-//
+/*
+ * Telegram V
+ * Copyright (C) 2020 original authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 import isaac from '../../../../vendor/isaac'
 export function mt_write_bytes(offset, length, input, buffer) {
     for (let i = 0; i < length; ++i) {
