263979bbec4453b6b5d274ba152169fe55274923
prettydude <Coolboy.mc1@gmail.com>
2020-05-27 00:03:39 +0300

    Optimized wallpaper preview

--- src/js/Ui/Components/Sidebars/Left/Settings/Background/SquareComponent.js
+++ src/js/Ui/Components/Sidebars/Left/Settings/Background/SquareComponent.js
@@ -2,30 +2,31 @@ import UIEvents from "../../../../../EventBus/UIEvents"
 import StatefulComponent from "../../../../../../V/VRDOM/component/StatefulComponent"
 
 class SquareComponent extends StatefulComponent {
-    state = {
+    /*state = {
         url: undefined
-    };
+    };*/
 
-    appEvents(E) {
+    /*appEvents(E) {
         E.bus(UIEvents.General)
             .filter(event => event.id === this.props.wallpaperId)
-            .on("wallpaper.ready", this.onWallpaperReady);
-    }
+            .on("wallpaper.previewReady", this.onWallpaperReady);
+    }*/
 
     render() {
         return (
             <div class="image-square"
                  onClick={this.props.click}
-                 url={this.state.url}
-                 style={{"background-image": `url(${this.state.url})`}}/>
+                 url={this.props.src}
+                 wallpaper={this.props.wallpaper}
+                 style={{"background-image": `url(${this.props.src})`}}/>
         );
     }
 
-    onWallpaperReady = event => {
+    /*onWallpaperReady = event => {
         this.setState({
             url: event.wallpaperUrl
         });
-    }
+    }*/
 }
 
 export default SquareComponent;
--- src/js/Ui/Managers/WallpaperManager.js
+++ src/js/Ui/Managers/WallpaperManager.js
@@ -16,6 +16,7 @@
  */
 
 import FileManager from "../../Api/Files/FileManager"
+import {FileAPI} from "../../Api/Files/FileAPI"
 import MTProto from "../../MTProto/External"
 import AppEvents from "../../Api/EventBus/AppEvents"
 import UIEvents from "../../Ui/EventBus/UIEvents"
@@ -27,70 +28,101 @@ class WallpaperManagerSingleton {
 
     init() {
         AppEvents.Files.subscribe("fileDownloaded", this.onFileDownloaded);
+
         keval.getItem("background").then(data => {
-            if(!data) return;
-            if(data.blob) {
+            if (!data) return;
+            if (data.blob) {
                 let url = URL.createObjectURL(data.blob);
                 this.setWallpaper(url);
-            } else if(data.color) {
+            } else if (data.color) {
                 this.setColor(data.color);
             }
         })
     }
 
     fetchAllWallPapers() {
-        if (this.wallpapers.length > 0 || this.isFetching) {
+        if (this.isFetching) {
+            return this.fetchPromise;
+        }
+
+        if (this.wallpapers.length > 0) {
             return Promise.resolve(this.wallpapers);
         }
 
         this.isFetching = true;
 
-        return MTProto.invokeMethod("account.getWallPapers", {hash: 0}).then(result => {
-            this.isFetching = true;
+        return this.fetchPromise = MTProto.invokeMethod("account.getWallPapers", { hash: 0 }).then(result => {
 
             this.wallpapers = result.wallpapers;
+            console.log(this.wallpapers);
+            this.isFetching = false;
 
-            UIEvents.General.fire("wallpaper.fetched", {wallpapers: this.wallpapers});
+            UIEvents.General.fire("wallpaper.fetched", { wallpapers: this.wallpapers });
 
-            this.wallpapers.forEach(wallpaper => {
+            /*this.wallpapers.forEach(wallpaper => { // don't download with fetching
                 FileManager.downloadDocument(wallpaper.document, undefined, true);
-            });
+            });*/
 
             return this.wallpapers;
         });
     }
 
+    async fetchPreview(wallpaper) {
+        return FileAPI.photoThumbnail(wallpaper.document).then(thumb => {
+            return thumb.src;
+        });
+    }
+
+    requestFull(wallpaper) {
+        FileManager.downloadDocument(wallpaper.document, undefined, true);
+    }
+
+    requestAndInstall(wallpaper) {
+        this.requestFull(wallpaper);
+        this.currentWallpaper = wallpaper;
+    }
+
     setWallpaper(url) {
-        if(!url) {
+        if (!url) {
             window.document.documentElement.style.removeProperty("--chat-bg-image");
             return;
         }
         window.document.documentElement.style.setProperty("--chat-bg-image", `url(${url})`);
         fetch(url).then(async response => {
             let blob = await response.blob();
-            keval.setItem("background", {blob: blob});
+            keval.setItem("background", { blob: blob });
         })
     }
 
     setColor(hex) {
-        if(!hex) {
+        if (!hex) {
             window.document.documentElement.style.removeProperty("--chat-bg-color");
             return;
         }
         this.setWallpaper(undefined); //remove wallpaper
         window.document.documentElement.style.setProperty("--chat-bg-color", hex);
-        keval.setItem("background", {color: hex});
+        keval.setItem("background", { color: hex });
     }
 
     onFileDownloaded = event => {
         if (!this.wallpapers.find(w => w.document.id === event.fileId)) {
             return;
         }
+        if (event.thumbSize) {
+            UIEvents.General.fire("wallpaper.previewReady", {
+                id: event.fileId,
+                wallpaperUrl: event.url
+            });
+        } else {
+            if(this.currentWallpaper?.document.id === event.fileId) {
+                this.setWallpaper(event.url); //apply full wallpaper
+            }
 
-        UIEvents.General.fire("wallpaper.ready", {
-            id: event.fileId,
-            wallpaperUrl: event.url
-        });
+            UIEvents.General.fire("wallpaper.fullReady", {
+                id: event.fileId,
+                wallpaperUrl: event.url
+            });
+        }
     }
 }
 
