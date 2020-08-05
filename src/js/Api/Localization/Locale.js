222b79d87b9b040e75e8db17dba4b466d1302230
prettydude <Coolboy.mc1@gmail.com>
2020-08-06 01:20:54 +0300

    Whoops

--- src/js/Api/Localization/Locale.js
+++ src/js/Api/Localization/Locale.js
@@ -2,6 +2,8 @@ import MTProto from "../../MTProto/External";
 import API from "../Telegram/API"
 import keval from "../../Keval/keval";
 import UIEvents from "../../Ui/EventBus/UIEvents"
+import VRNode from "../../V/VRDOM/VRNode"
+import VRDOM from "../../V/VRDOM/VRDOM"
 import * as makePlural from "make-plural"
 
 class Locale {
@@ -37,33 +39,73 @@ class Locale {
 		UIEvents.General.fire("language.ready");
 	}
 
-	/*
-		* Method for non-plural strings
+	/**
+		Method for non-plural strings
+		@returns Array of String or VRNode, if any VRNode passed as replacement
 	*/
 	l(key, replaces) {
 		return this.lp(key, 1, replaces); //return string for one, if plural key passed
 	}
 
-	/*
-		* Method for plural strings
+	/**
+		Method for plural strings
+		@returns Array of String or VRNode, if any VRNode passed as replacement
 	*/
 	lp(key="NO_TRANSLATION_KEY_PROVIDED", count=1, replaces = {}) {
 		if(key.key) return this.lp(key.key, key.count, key.replaces); //if object passed
 
 		let value = this.strings?.get(key);
 		if(!value) return key;
+
 		let found = key;
 		if(!value._plural) {
 			found = value;
 		} else {
 			found = value[this.countToPluralCode(count)] || value.other; 
 		}
+
 		for(let replace in replaces) {
-			let text = replaces[replace];
-			if(text.key) text = this.lp(text.key, text.count, text.replaces); //allow recursive replaces
-			found = found.replace(`{${replace}}`, text);
+			//replacement could be: String, Replacement, VRNode
+			let replacement = replaces[replace];
+			if(replacement.key) replacement = this.lp(replacement.key, replacement.count, replacement.replaces); //allow recursive replaces
+			found = this._replace(found, replace, replacement);
 		}
+
+		if(!Array.isArray(found)) {
+			found = [found] //wrap in Array, for return consistency 
+		}
+
 		return found;
+	}
+
+	_replace(where, what, to) {
+		if(!Array.isArray(where)) where = [where];
+		if(!Array.isArray(to)) to = [to];
+
+		let replaced = [];
+
+		for(let part of where) {
+			if(typeof part !== "string") {
+				replaced.push(part);
+				continue;
+			}
+
+			for(let replacement of to) {
+				if(replacement instanceof VRNode) {
+					const position = part.indexOf(`{${what}}`);
+					if(position === -1) continue;
+					let split = [];
+					split[0] = part.substring(0, position);
+					split[1] = to;
+					split[2] = part.substring(position + `{${what}}`.length);
+					part = split;
+				} else {
+					part = part.replace(`{${what}}`, replacement)
+				}
+			}
+			replaced.push(part);
+		}
+		return replaced.flat();
 	}
 
 	get currentLanguageCode() {
