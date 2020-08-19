26632bfb406e9e05a2c2ea41d8a378bb31c41107
Davyd Kohut <expexes@gmail.com>
2020-08-19 15:11:40 +0300

    disable longtapplugin

--- src/js/Ui/Plugins/LongtapPlugin.js
+++ src/js/Ui/Plugins/LongtapPlugin.js
@@ -2,43 +2,43 @@ import VRDOMPlugin from "../../V/VRDOM/plugin/VRDOMPlugin"
 
 class LongtapVRDOMPlugin extends VRDOMPlugin {
     elementDidMount($el) {
-        if ($el.nodeType !== Node.TEXT_NODE && $el.oncontextmenu) {
-            let timeout = 0;
-
-            ["mousedown", "touchstart"].forEach(ev => {
-                $el.addEventListener(ev, function(e) {
-                	if(!(e.isTrusted || e.which === 1)) return; //only left click
-                    clearTimeout(timeout);
-                    $el.classList.add("no-select");
-
-                    let rect = $el.getBoundingClientRect();
-
-                    timeout = setTimeout(_ => {
-                        let event = new MouseEvent('contextmenu', {
-                            clientX: e.clientX || rect.left + rect.width / 2,
-                            clientY: e.clientY || rect.top + rect.height / 2,
-                        });
-                        $el.dispatchEvent(event);
-                        $el.classList.remove("no-select");
-                    }, 500)
-
-                }, { passive: true })
-            });
-
-            ["mousemove", "touchmove"].forEach(ev => {
-                $el.addEventListener(ev, function(e) {
-                    clearTimeout(timeout);
-                    $el.classList.remove("no-select");
-                }, { passive: true });
-            });
-
-            ["mouseup", "touchend"].forEach(ev => {
-                $el.addEventListener(ev, function(e) {
-                    clearTimeout(timeout);
-                    $el.classList.remove("no-select");
-                }, { passive: true });
-            });
-        }
+        // if ($el.nodeType !== Node.TEXT_NODE && $el.oncontextmenu) {
+        //     let timeout = 0;
+        //
+        //     ["mousedown", "touchstart"].forEach(ev => {
+        //         $el.addEventListener(ev, function(e) {
+        //         	if(!(e.isTrusted || e.which === 1)) return; //only left click
+        //             clearTimeout(timeout);
+        //             $el.classList.add("no-select");
+        //
+        //             let rect = $el.getBoundingClientRect();
+        //
+        //             timeout = setTimeout(_ => {
+        //                 let event = new MouseEvent('contextmenu', {
+        //                     clientX: e.clientX || rect.left + rect.width / 2,
+        //                     clientY: e.clientY || rect.top + rect.height / 2,
+        //                 });
+        //                 $el.dispatchEvent(event);
+        //                 $el.classList.remove("no-select");
+        //             }, 500)
+        //
+        //         }, { passive: true })
+        //     });
+        //
+        //     ["mousemove", "touchmove"].forEach(ev => {
+        //         $el.addEventListener(ev, function(e) {
+        //             clearTimeout(timeout);
+        //             $el.classList.remove("no-select");
+        //         }, { passive: true });
+        //     });
+        //
+        //     ["mouseup", "touchend"].forEach(ev => {
+        //         $el.addEventListener(ev, function(e) {
+        //             clearTimeout(timeout);
+        //             $el.classList.remove("no-select");
+        //         }, { passive: true });
+        //     });
+        // }
     }
 }
 
