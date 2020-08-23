45f199f0ff1df7a6350a80c11b2eebd40cbf63c7
prettydude <Coolboy.mc1@gmail.com>
2020-08-23 10:40:26 +0300

    Context menu on mobile

--- src/js/Ui/Plugins/LongtapPlugin.js
+++ src/js/Ui/Plugins/LongtapPlugin.js
@@ -1,16 +1,15 @@
 import VRDOMPlugin from "../../V/VRDOM/plugin/VRDOMPlugin"
 import { IS_MOBILE_SCREEN } from "../../Utils/browser"
 
-const LONG_TAP_DURATION_MS = 250;
+const LONG_TAP_DURATION_MS = 500;
 
 class LongtapVRDOMPlugin extends VRDOMPlugin {
     elementDidMount($el) {
         if (!IS_MOBILE_SCREEN) return;
 
-        if ($el.nodeType !== Node.TEXT_NODE && $el.oncontextmenu) {
+        if ($el.nodeType !== Node.TEXT_NODE && $el.oncontextmenu && !$el.isContentEditable) { // we don't support contenteditable elements
             ["mousedown", "touchstart"].forEach(this.handleStart($el));
-            ["mousemove", "touchmove"].forEach(this.handleMove($el));
-            ["mouseup", "touchend"].forEach(this.handleEnd($el));
+            ["mousemove", "mouseup", "touchmove", "touchcancel", "touchend"].forEach(this.cancelTimer($el));
         }
     }
 
@@ -18,49 +17,50 @@ class LongtapVRDOMPlugin extends VRDOMPlugin {
         return (ev) => {
             const cancel = this.cancel;
             $el.addEventListener(ev, e => {
-                if (e.which !== 1) return;
-
                 cancel($el);
-                console.log("added")
+
                 document.getElementById("app").classList.add("no-select");
 
                 let rect = $el.getBoundingClientRect();
+                const loc = e.touches ? e.touches[0] : e;
 
                 let timeout = setTimeout(_ => {
+                    // cancel next click, to fix Android
+                    $el.addEventListener('touchend', function cancelClick(e) {
+                        element.removeEventListener('touchend', cancelClick, true);
+                        e.stopImmediatePropagation();
+                        e.preventDefault();
+                        e.stopPropagation();
+                    }, true);
+
+                    const pageTop = window.visualViewport.pageTop // fix for open keyboard on IOS
+
                     let event = new MouseEvent('contextmenu', {
-                        clientX: e.clientX || rect.left + rect.width / 2,
-                        clientY: e.clientY || rect.top + rect.height / 2,
+                        clientX: loc.clientX || rect.left + rect.width / 2,
+                        clientY: (loc.clientY || rect.top + rect.height / 2) + pageTop/2,
                     });
                     $el.dispatchEvent(event);
-                    $el.classList.remove("no-select");
-                }, 500)
-                $el.setAttribute("longtap-timeout", timeout);
 
+                    document.getElementById("app").classList.remove("no-select");
+                }, LONG_TAP_DURATION_MS)
+                $el.setAttribute("longtap-timeout", timeout);
+                return true;
             }, { passive: true })
         }
     }
 
-    handleMove = $el => {
-        const cancel = this.cancel;
+    cancelTimer = $el => {
         return (ev) => {
             $el.addEventListener(ev, e => {
-                cancel($el);
-            }, { passive: true });
-        }
-    }
-
-    handleEnd = $el => {
-        const cancel = this.cancel;
-        return (ev) => {
-            $el.addEventListener(ev, e => {
-                cancel($el);
+                this.cancel($el);
             }, { passive: true });
         }
     }
 
     cancel = ($el) => {
+        console.log("cancel")
         const timeout = $el.getAttribute("longtap-timeout");
-        if(timeout) {
+        if (timeout) {
             clearTimeout(Number.parseInt(timeout));
         }
         document.getElementById("app").classList.remove("no-select");
