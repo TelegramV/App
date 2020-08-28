561cff833cb015b88f4c3dc3022eb6a64608c00b
A H A <annvass00@gmail.com>
2020-08-28 10:46:07 +0300

    cleanup

--- src/js/MTProto/Cryptography/mt_srp.js
+++ src/js/MTProto/Cryptography/mt_srp.js
@@ -20,7 +20,6 @@
 import {mt_write_bytes} from "./mt_inob_codec"
 import BigInteger from "big-integer"
 import Uint8 from "../Utils/Uint8"
-import sha256 from "./sha256"
 
 async function H(data) {
     //console.log(new Uint8Array(await crypto.subtle.digest("SHA-256", data)), data)
