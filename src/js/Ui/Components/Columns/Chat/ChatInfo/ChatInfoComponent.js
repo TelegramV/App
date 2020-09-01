8de5a5d0cf97a30e8f7c86c8ae10617ca02419ed
prettydude <Coolboy.mc1@gmail.com>
2020-09-01 23:26:29 +0300

    Unread counter on mobile

--- src/js/Ui/Components/Columns/Chat/ChatInfo/ChatInfoComponent.js
+++ src/js/Ui/Components/Columns/Chat/ChatInfo/ChatInfoComponent.js
@@ -4,22 +4,30 @@ import AppSelectedChat from "../../../../Reactive/SelectedChat"
 import AppSelectedInfoPeer from "../../../../Reactive/SelectedInfoPeer";
 import VApp from "../../../../../V/vapp"
 import UIEvents from "../../../../EventBus/UIEvents"
+import AppEvents from "../../../../../Api/EventBus/AppEvents"
 import AvatarComponent from "../../../Basic/AvatarComponent"
 import StatelessComponent from "../../../../../V/VRDOM/component/StatelessComponent"
+import FoldersManager from "../../../../../Api/Dialogs/FolderManager";
 
 class ChatInfoComponent extends StatelessComponent {
 
     appEvents(E: AE) {
         E.bus(UIEvents.General)
             .on("chat.select", this.onChatSelect)
+        E.bus(AppEvents.Dialogs)
+            .updateOn("gotMany")
+            .updateOn("gotNewMany")
     }
 
     render() {
+        const unreadCount = FoldersManager.getBadgeCount(null)?.count
         return (
             <div id="messages-wrapper-chat-info" className="chat-info">
                 <div className="person">
 
-                    <div class="responsive-only-mobile btn-icon tgico-back" onClick={this.backToMainPage}/>
+                    <div class="responsive-only-mobile btn-icon tgico-back" onClick={this.backToMainPage}>
+                        {unreadCount && <div class="unread-badge">{unreadCount}</div>}
+                    </div>
 
                     <AvatarComponent peer={AppSelectedChat.current}/>
 
